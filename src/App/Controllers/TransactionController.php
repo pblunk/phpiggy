4d3f5e0fbc579a5f29c579d0fadbd352ec6ed932
Phil Blunk <philipblunk@gmail.com>
2025-02-16 17:06:14 -0600

    Transaction Setup
    Setting up the transaction page

---
+++ src/App/Controllers/TransactionController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use Framework\TemplateEngine;
+use App\Services\{ValidatorService, TransactionService};
+
+class TransactionController
+{
+    public function __construct(
+        private TemplateEngine $view,
+        private ValidatorService $validatorService,
+        private TransactionService $transactionService
+    ) {}
+
+    public function createView()
+    {
+        echo $this->view->render("transactions/create.php");
+    }
+
+    public function create()
+    {
+        $this->validatorService->validateTransaction($_POST);
+
+        $this->transactionService->create($_POST);
+
+        redirectTo('/');
+    }
+}
